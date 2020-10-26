452dec7fc79857096810b8e2b192d8a6e4465dd2
Daniel <daniel.laskowski94@gmail.com>
2020-10-26 22:14:41 +0100

    add register, login and reset pass functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Dashboard from './components/Dashboard'
+import Signup from './components/Signup';
+import Login from './components/Login'
+import ForgotPassword from './components/ForgotPassword'
+import PrivateRoute from './components/PrivateRoute'
+import { AuthProvider } from './context/AuthContext'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container className="d-flex align-items-center justify-content-center" styles={{ minHeight: "100vh" }}>
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Router>
+          <AuthProvider>
+            <Switch>
+              <PrivateRoute exact path="/" component={Dashboard} />
+              <Route path="/signup" component={Signup} />
+              <Route path="/login" component={Login} />
+              <Route path="/forgot-password" component={ForgotPassword} />
+            </Switch>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
+  )
 }
 
 export default App;
